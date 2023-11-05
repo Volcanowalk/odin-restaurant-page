f8551596cae183431398fdf4b56d2aa061de32bd
Volcanowalk <leedylan7989@gmail.com>
2023-11-05 15:48:55 -0500

    Improved UI

--- src/home.js
+++ src/home.js
@@ -1,11 +1,13 @@
 const displayHome = () => {
     const div_main = document.querySelector('.div-main')
 
+    //Greeting text
     const txtGreeting = document.createElement('p')
     txtGreeting.classList.add('txtGreeting')
     txtGreeting.innerText = 'WELCOME TO GARDEN'
     div_main.appendChild(txtGreeting)
 
+    //Text area below greeting text
     const txtDetail = document.createElement('div')
     txtDetail.classList.add('txtDetail')
     txtDetail.innerText = 'Garden restaurant offers various plants, flowers, and seeds for forest animals! \n\n Humans are also welcomed here, but they rarely like our menu.'
